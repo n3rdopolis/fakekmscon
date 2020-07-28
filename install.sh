99ab17e486c6832ed5d4a25278b0f33a2e47f894
nerdopolis <bluescreen_avenger@verizon.net>
2020-07-27 23:57:27 -0400

    sync up

--- install.sh
+++ install.sh
@@ -6,17 +6,12 @@ cp -r "$SCRIPTFOLDERPATH"/usr/* /usr
 cp -r "$SCRIPTFOLDERPATH"/etc/* /etc
 systemctl enable vtty-backend@.service
 systemctl enable vtty-frontend@.service
-chmod +x /usr/bin/vtty-backend
-chmod +x /usr/bin/vtty-frontend
+#systemctl enable vtty-monitor@.service 
+chmod +x /usr/libexec/vtty-backend
+chmod +x /usr/libexec/vtty-frontend
+chmod +x /usr/libexec/vtty-monitor
+chmod +x /usr/bin/vtty-toggle
 
 
 ln -s /usr/lib/systemd/system/vtty-frontend@.service /etc/systemd/system/autovt@.service
 
-systemd-sysusers
-
-which tmux &> /dev/null
-if [[ $? != 0 ]]
-then
-  sudo apt-get update
-  sudo apt-get install tmux
-fi
