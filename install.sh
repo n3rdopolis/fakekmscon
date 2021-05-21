27c8333d5fa517d8be21cf9f85939d7693619d4a
nerdopolis <bluescreen_avenger@verizon.net>
2021-05-21 19:35:03 -0400

    Newer iteration of vTTYs. socat over tmux. creation of a seat-vtty seat, login sessions run on seat-vtty

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@ SCRIPTFOLDERPATH=$(dirname "$SCRIPTFILEPATH")
 
 cp -r "$SCRIPTFOLDERPATH"/usr/* /usr
 cp -r "$SCRIPTFOLDERPATH"/etc/* /etc
-systemctl enable vtty-backend@.service
 systemctl enable vtty-frontend@.service
-#systemctl enable vtty-monitor@.service 
 chmod +x /usr/libexec/vtty-backend
 chmod +x /usr/libexec/vtty-frontend
-chmod +x /usr/libexec/vtty-monitor
+chmod +x /usr/libexec/vtty-login
+chmod +x /usr/libexec/vtty-connect
 chmod +x /usr/bin/vtty-toggle
 
 
